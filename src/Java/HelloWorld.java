bdf95fa468b4483a41784cc28e0e727ad1c8d3eb
zhushushu <zhu_shuzhen@126.com>
2017-07-08 17:33:37 +0800

    HelloWorld

---
+++ src/Java/HelloWorld.java
@@ -0,0 +1,10 @@
+package Java;
+
+/**
+ * Created by zsz on 2017/7/8.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
